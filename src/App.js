387ee0ef23464a4f6e24013640be42366cf0782d
Walid Arouss <warouss@aliznet.fr>
2021-07-05 16:59:31 +0200

    How to Pass Data to Components with Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./styles.css";
 
 export default function App() {
+  const todos = [
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ];
+
   return (
-    <div className="App">
+    <div>
       <h1>Todo List</h1>
+      <TodoList todos={todos} />
     </div>
+  );
+}
+
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map((todo) => (
+        <li>{todo.text}</li>
+      ))}
+    </ul>
   );
 }

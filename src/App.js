e15d7de9b7e5a8b6bc369df3e9bcb712f28c2862
Walid Arouss <warouss@aliznet.fr>
2021-07-05 18:03:10 +0200

    How to Handle Deleting Todos

--- src/App.js
+++ src/App.js
@@ -17,6 +17,32 @@ export default function App() {
   );
 }
 
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((t) => t.id !== todo.id);
+      });
+    }
+  }
+
+  return (
+    <span
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer"
+      }}
+    >
+      x
+    </span>
+  );
+}
+
 function TodoList({todos, setTodos}) {
   function handleToggleTodo(todo) {
     // confused by this code? Here's what it says:
@@ -40,9 +66,12 @@ function TodoList({todos, setTodos}) {
     <ul>
       {todos.map((todo) => (
         <li 
-        onClick={() => handleToggleTodo(todo)}
+        onDoubleClick={() => handleToggleTodo(todo)}
         style={{textDecoration: todo.done ? 'line-through' : ''}}
-        key={todo.id}>{todo.text}</li>
+        key={todo.id}>
+          {todo.text}
+          <DeleteTodo todo={todo} setTodos={setTodos}/>
+        </li>
       ))}
     </ul>
   );
@@ -55,7 +84,7 @@ function AddTodo({ setTodos }) {
     event.preventDefault(); // This method prevents the default action whenever we submit a form
     const text = event.target.elements.addTodo.value;
     const todo = {
-      id: 4,
+      id: Math.random(),
       text,
       done: false
     };
